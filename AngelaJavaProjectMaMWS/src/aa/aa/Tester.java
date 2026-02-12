0698509e26f1f57b4e8036b4c6b0265b4810b205
atopp <atopp@AngelaAcerHome>
2026-02-12 08:29:54 -0600

    checkin

--- AngelaJavaProjectMaMWS/src/aa/aa/Tester.java
+++ AngelaJavaProjectMaMWS/src/aa/aa/Tester.java
@@ -5,15 +5,26 @@ public class Tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String value = "40"; //final value should be 0000000040  size10
-		System.out.println(appendValues("40", 10, "0"));
-		
-		getFirstValue();
-		
-		System.out.println(convertStringToInt("123"));
-		
-		System.out.println("testTruncate="+truncate("9111111110922222222033333333309444444440",20));
+//		String value = "40"; //final value should be 0000000040  size10
+//		System.out.println(appendValues("40", 10, "0"));
+//		
+//		getFirstValue();
+//		
+//		System.out.println(convertStringToInt("123"));
+//		
+//		System.out.println("testTruncate="+truncate("9111111110922222222033333333309444444440",20));
 
+		System.out.println(calcCatchWght("001","15000","4"));
+	}
+	
+	public static String calcCatchWght(String asnSeqNbr, String wght, String ilpnReceived) {
+		
+		
+		int value1= Integer.parseInt(ilpnReceived);
+		int value2=Integer.parseInt(ilpnReceived);
+		
+		int calcValue = Integer.parseInt(wght)/Integer.parseInt(ilpnReceived);
+		return asnSeqNbr+"|"+String.valueOf(calcValue);
 	}
 	public static String truncate(String value, int maxSize) {
 		
